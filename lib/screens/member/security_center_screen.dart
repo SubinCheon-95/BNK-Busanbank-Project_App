7b58cb48bc77fd33224fad375365585a017dd414
o-seoj <tjwjd010@naver.com>
2026-01-02 17:34:25 +0900

    [26/01/02] member/인증센터 배경 수정/by o-seoj

--- lib/screens/member/security_center_screen.dart
+++ lib/screens/member/security_center_screen.dart
@@ -307,7 +307,7 @@ class _SecurityItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.08),
+              color: Colors.black.withOpacity(0.04),
               blurRadius: 8,
               offset: const Offset(0, 4),
             ),
